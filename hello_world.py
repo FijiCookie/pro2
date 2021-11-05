2b7019616d9362e7c65fb887990b2d92f72c8c26
FijiCookie <91664070+FijiCookie@users.noreply.github.com>
2021-11-05 17:42:41 +0100

    preparing for merge with externally created readme on github

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,21 @@
-test = "fiji cookie queen"
-print(test)
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask("Hello World")
+
+@app.route("/hello/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        ziel_nahrungsmittel = request.form['artikel']
+        rueckgabe_string = ziel_nahrungsmittel
+
+        ziel_menge = request.form['menge']
+        rueckgabe_string = ziel_menge
+
+        return render_template("foodstorage.html", artikelausgabe=rueckgabe_string)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
